506e04ee2e9610e472dba5284dbb2d97e2d99b4a
Amanda Hanssen <2345019@laconiaschools.org>
2021-03-10 12:56:44 -0500

    -

---
+++ pass_return.py
@@ -0,0 +1,23 @@
+def harold():
+    print('Hello Harry')
+    return True
+    
+def hello_func():
+    print("good bye")
+    
+print('Hello jumper! 1')
+print('Hello jumper! 2')
+print('Hello jumper! 3')
+print('Hello jumper! 4')
+print('Hello jumper! 5')
+
+
+hello_func()
+
+if 1 < 5:
+    pass
+
+
+if harold():
+    print("thats NOT my name!")
+ 
